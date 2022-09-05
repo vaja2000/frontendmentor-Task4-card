3cf30b2f6ac18e26509adc2fbdec5f2fc2f8d30c
vaja <74463035+vaja2000@users.noreply.github.com>
2022-09-06 02:09:10 +0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Task4';
+  cardHolderNameFormControl:any
+  cardData:FormGroup = new FormGroup({})
+  date:any = new Date().getFullYear()
+
+  ngOnInit(): void {
+    this.cardData = new FormGroup({
+      'CardHolderName' : new FormControl( null, [
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.pattern("[a-zA-Z]{1,9}")
+      ] ),
+      'CardNumber' : new FormControl( null, [
+        Validators.required,
+        Validators.pattern("[0-9]{16}")
+      ] ),
+      'Month' : new FormControl( null, [
+        Validators.required,
+        Validators.pattern("[1-9]{1,2}"),
+        Validators.max(12)
+      ] ),
+      'Year' : new FormControl( null, [
+        Validators.required,
+        Validators.pattern("[0-9]{4}"),
+        Validators.min(this.date - 4),
+        Validators.max(this.date + 4)
+      ] ),
+      'CVC' : new FormControl( null, [
+        Validators.required,
+        Validators.pattern("[0-9]{3}")
+      ] )
+    })
+  }
+  CardNumberSpace(): string {
+    let num = String(this.cardData.get('CardNumber')?.value)
+    let result:string = ''
+    let count:number = 3
+    for(let symbol of num) {
+      result += symbol
+      if(count == 0) {
+        result += " "
+        count = 4
+      }
+      count --
+    }
+    return result
+  }
+  onSubmit() {
+    console.log(this.CardNumberSpace())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
